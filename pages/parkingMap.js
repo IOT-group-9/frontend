52f558df15438e453688d4a2f4bfd34c1c4758ae
John Mo <peikaimo@gmail.com>
2024-12-02 18:37:28 +0800

    fixed parking position, was top y1

--- pages/parkingMap.js
+++ pages/parkingMap.js
@@ -11,25 +11,25 @@ const ParkingMap = () => {
   useEffect(() => {
     const fetchParkingData = async () => {
       //     should return this
-      // let mockParkingData = [
-      //     { 'id': 1, 'x1': 0, 'y1': 0, 'x2': 100, 'y2': 150, 'isOccupied': true },
-      //     { 'id': 2, 'x1': 125, 'y1': 0, 'x2': 225, 'y2': 150, 'isOccupied': false },
-      //     { 'id': 3, 'x1': 250, 'y1': 0, 'x2': 350, 'y2': 150, 'isOccupied': true },
-      //     { 'id': 4, 'x1': 375, 'y1': 0, 'x2': 475, 'y2': 150, 'isOccupied': false },
-      //     { 'id': 5, 'x1': 500, 'y1': 0, 'x2': 600, 'y2': 150, 'isOccupied': true },
-      //     { 'id': 6, 'x1': 625, 'y1': 0, 'x2': 725, 'y2': 150, 'isOccupied': false },
-      //     { 'id': 7, 'x1': 750, 'y1': 0, 'x2': 850, 'y2': 150, 'isOccupied': true },
-      //     { 'id': 8, 'x1': 875, 'y1': 0, 'x2': 975, 'y2': 150, 'isOccupied': false },
-      //     { 'id': 9, 'x1': 0, 'y1': 175, 'x2': 100, 'y2': 325, 'isOccupied': true },
-      //     { 'id': 10, 'x1': 125, 'y1': 175, 'x2': 225, 'y2': 325, 'isOccupied': false },
-      //     { 'id': 11, 'x1': 250, 'y1': 175, 'x2': 350, 'y2': 325, 'isOccupied': true },
-      //     { 'id': 12, 'x1': 375, 'y1': 175, 'x2': 475, 'y2': 325, 'isOccupied': false },
-      //     { 'id': 13, 'x1': 500, 'y1': 175, 'x2': 600, 'y2': 325, 'isOccupied': true },
-      //     { 'id': 14, 'x1': 625, 'y1': 175, 'x2': 725, 'y2': 325, 'isOccupied': false },
-      //     { 'id': 15, 'x1': 750, 'y1': 175, 'x2': 850, 'y2': 325, 'isOccupied': true },
-      //     { 'id': 16, 'x1': 875, 'y1': 175, 'x2': 975, 'y2': 325, 'isOccupied': false }
-      // ];
-      let mockParkingData = (await (await fetch(`https://${HOST}/api/sensor/mapslot?map=${MAP}&__order=id&__page_size=16&__range_header=true`)).json()).rows;
+      let mockParkingData = [
+        { 'id': 1, 'x1': 0, 'y1': 0, 'x2': 100, 'y2': 150, 'occupied': true },
+        { 'id': 2, 'x1': 125, 'y1': 0, 'x2': 225, 'y2': 150, 'occupied': false },
+        { 'id': 3, 'x1': 250, 'y1': 0, 'x2': 350, 'y2': 150, 'occupied': true },
+        { 'id': 4, 'x1': 375, 'y1': 0, 'x2': 475, 'y2': 150, 'occupied': false },
+        { 'id': 5, 'x1': 500, 'y1': 0, 'x2': 600, 'y2': 150, 'occupied': true },
+        { 'id': 6, 'x1': 625, 'y1': 0, 'x2': 725, 'y2': 150, 'occupied': false },
+        { 'id': 7, 'x1': 750, 'y1': 0, 'x2': 850, 'y2': 150, 'occupied': true },
+        { 'id': 8, 'x1': 875, 'y1': 0, 'x2': 975, 'y2': 150, 'occupied': false },
+        { 'id': 9, 'x1': 0, 'y1': 175, 'x2': 100, 'y2': 325, 'occupied': true },
+        { 'id': 10, 'x1': 125, 'y1': 175, 'x2': 225, 'y2': 325, 'occupied': false },
+        { 'id': 11, 'x1': 250, 'y1': 175, 'x2': 350, 'y2': 325, 'occupied': true },
+        { 'id': 12, 'x1': 375, 'y1': 175, 'x2': 475, 'y2': 325, 'occupied': false },
+        { 'id': 13, 'x1': 500, 'y1': 175, 'x2': 600, 'y2': 325, 'occupied': true },
+        { 'id': 14, 'x1': 625, 'y1': 175, 'x2': 725, 'y2': 325, 'occupied': false },
+        { 'id': 15, 'x1': 750, 'y1': 175, 'x2': 850, 'y2': 325, 'occupied': true },
+        { 'id': 16, 'x1': 875, 'y1': 175, 'x2': 975, 'y2': 325, 'occupied': false }
+      ];
+      // let mockParkingData = (await (await fetch(`https://${HOST}/api/sensor/mapslot?map=${MAP}&__order=id&__page_size=16&__range_header=true`)).json()).rows;
       setParkingSlots(mockParkingData);
     };
     fetchParkingData();
@@ -105,7 +105,7 @@ const ParkingMap = () => {
             className={styles.parkingSlot}
             style={{
               left: `${slot.x1}px`,
-              top: `${slot.y2}px`,
+              top: `${slot.y1}px`,
               width: `${slot.x2 - slot.x1}px`,
               height: `${slot.y2 - slot.y1}px`,
               backgroundColor: slot.occupied ? '#FFB6B3' : '#BDE7BD'
